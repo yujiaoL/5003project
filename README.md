9e959324ac2b3a44e3492a00ec1feea3c17afd0d
liuyujiao <yujiaoliu3-c@my.cityu.edu.hk>
2024-12-06 22:12:37 +0800

    1

---
+++ README.md
@@ -0,0 +1,20 @@
+# flask项目
+
+## 实现功能
+
+1. 用户登录注册
+2. 增删改查用户博文
+
+## 主要技术栈
+
+1. python 3.11
+2. flask
+3. SQLite3
+4. Jinjia
+
+## 使用方式
+
+根目录下运行以下命令
+
+1. 初始化数据库 `flask --app flaskr init-db`
+2. 运行项目 `flask  --app flaskr run --debug`
---
+++ flaskr/schema.sql
@@ -0,0 +1,61 @@
+DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS Post;
+DROP TABLE IF EXISTS Tag;
+DROP TABLE IF EXISTS Post_Tag;
+
+CREATE TABLE User
+(
+    id       INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT        NOT NULL,
+    permission INTEGER,
+    introduction TEXT
+);
+
+CREATE TABLE Post
+(
+    id        INTEGER PRIMARY KEY AUTOINCREMENT,
+    author_id INTEGER   NOT NULL,
+    created_time   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    title     TEXT      NOT NULL,
+    body      TEXT      NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES User (id)
+);
+
+CREATE TABLE Tag
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE Post_Tag
+(
+    pid INTEGER,
+    tid INTEGER,
+    PRIMARY KEY(pid,tid),
+    FOREIGN KEY (pid) REFERENCES Post (id),
+    FOREIGN KEY (tid) REFERENCES Tag (id)
+);
+
+CREATE TABLE Like
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    pid INTEGER,
+    uid INTEGER,
+    FOREIGN KEY (pid) REFERENCES Post (id),
+    FOREIGN KEY (uid) REFERENCES User (id)
+
+);
+
+CREATE TABLE Comment
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    pid INTEGER,
+    uid INTEGER,
+    content TEXT,
+    parent_id INTEGER,
+    comment_time  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (pid) REFERENCES Post (id),
+    FOREIGN KEY (uid) REFERENCES User (id),
+    FOREIGN KEY (parent_id) REFERENCES Comment (id)
+);
